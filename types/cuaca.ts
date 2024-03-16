33b3dfe01ee762be06eb4efc9a3c2ac189059af0
Azriel Akbar Ferry Ardiansyah Kusumawardhana <ferryakbarardiansyah@gmail.com>
2024-03-16 15:32:02 +0700

    chore(init): bmkg-api
    Signed-off-by: Azriel Akbar Ferry Ardiansyah Kusumawardhana <ferryakbarardiansyah@gmail.com>

---
+++ types/cuaca.ts
@@ -0,0 +1,9 @@
+export interface Cuaca {
+    tanggal: string;
+    kodeCuaca: string;
+    ketCuaca?: string;
+    suhu: string;
+    kelembapan: string;
+    arahAngin: string;
+    kecepatanAngin: string;
+}
